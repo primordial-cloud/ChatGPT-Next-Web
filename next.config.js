e56b6722fd17f9df68b6882800a97c316cfb9d4a
landfei <landfei@126.com>
2023-05-03 15:30:01 +0800

    add cdn

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  distDir: 'build',
+  assetPrefix: isProd ? 'https://cdn1.posts.chat/gpt' : undefined,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
