c0dfea994b1b6497a100bfb13fdafd3840fe8e26
landfei <landfei@126.com>
2023-05-16 20:25:07 +0800

    调整lang=zh-CN

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="zh-CN">
       <head>
         <meta
           name="theme-color"
